4de810c4715902c3b3e0e6d4c7bdd92211204b4d
DGBIZT <georgiy.gorlov@gmail.com>
2025-05-25 20:58:39 +0300

    Продолжение реализации 2 задания

--- data/hh.py
+++ data/hh.py
@@ -33,19 +33,49 @@ class HH(VacancyApi):
             else:
                 raise Exception(f"Ошибка при запросе к API: {response.status_code}")
 
+    def get_vacancies(self):
+        return self.__vacancies
+
 class Vacancy:
     def __init__(self, name_vacancy, url_vacancy, salary, town, description):
-        self.name_vacancy = name_vacancy
-        self.url_vacancy = url_vacancy
-        self.salary = salary
-        self.town = town
-        self.description = description
+        self.name_vacancy = name_vacancy.get("name")
+        self.url_vacancy = url_vacancy.get('alternate_url')
+        self.salary = salary.get('from') if salary else "Зарплата не указана"
+        self.town = town.get('name')
+        self.description = description.get('description')
+
+    def __str__(self):
+        return (f"{self.salary}")
+
 
 
 
 if __name__ == '__main__':
-    hh = HH()
-    # fgh = "vacancies.json"
-    # adf = HH(fgh)
-    hh.load_vacancies("Python")
+    # hh = HH()
+    # # fgh = "vacancies.json"
+    # # adf = HH(fgh)
+    # hh.load_vacancies('python')
+    # # all_vac = hh.get_vacancies()
+    # all_vacancy = Vacancy
+    # print(all_vacancy)
+    # Создаем экземпляр класса HH
+    hh_parser = HH()
+
+    # Загружаем вакансии
+    hh_parser.load_vacancies('водитель')
+
+    # Получаем список вакансий
+    vacancies_list = hh_parser.get_vacancies()
+
+    # Теперь можно работать со списком вакансий
+    for vacancy in vacancies_list:
+        # Создаем объекты класса Vacancy с обработкой возможных None значений
+        new_vacancy = Vacancy(
+            name_vacancy=vacancy,
+            url_vacancy=vacancy,
+            salary=vacancy.get('salary'),
+            town=vacancy.get('area'),
+            description=vacancy
+        )
+        print(new_vacancy)
 
